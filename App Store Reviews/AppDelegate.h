88ba0508ac016a9ada560241ac891bb89daa9528
Björn Kaiser <bjoern@aboalarm.de>
2013-03-02 13:46:10 +0100

    Initial Commit

---
+++ App Store Reviews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  App Store Reviews
+//
+//  Created by Björn Kaiser on 02.03.13.
+//  Copyright (c) 2013 Björn Kaiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App Store Reviews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  App Store Reviews
+//
+//  Created by Björn Kaiser on 02.03.13.
+//  Copyright (c) 2013 Björn Kaiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
